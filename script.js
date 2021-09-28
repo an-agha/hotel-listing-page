4b4294258bd18efdbb78c7bcd5fb8c2506b07d7e
an-agha <anaghanisakar@gmail.com>
2021-09-28 10:38:59 +0530

    pop up view

---
+++ script.js
@@ -0,0 +1,17 @@
+function showFilter()
+{
+    document.getElementById("nav_filter").style.height='100vh';
+}
+function hideFilter()
+{
+    document.getElementById("nav_filter").style.height='0vh';
+}
+
+function showSort()
+{
+    document.getElementById("nav_sort").style.height='55vh';
+}
+function hideSort()
+{
+    document.getElementById("nav_sort").style.height='0vh';
+}
